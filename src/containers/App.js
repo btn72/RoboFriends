f0fe49814ac94380cd9967cb9792edfebd50cf9d
btn72 <50052584+btn72@users.noreply.github.com>
2020-10-03 10:36:56 +0700

    Add files via upload

---
+++ src/containers/App.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react';
+import CartList from '../components/CartList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundary from '../components/ErrorBoundary';
+import './App.css';
+
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state={
+            robots: [],
+            searchField: ''
+        };
+    }
+
+    componentDidMount(){
+        fetch("http://jsonplaceholder.typicode.com/users")
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));
+    }
+
+    onSearchChange = (event) =>{
+        this.setState({searchField: event.target.value});
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLocaleLowerCase().includes(this.state.searchField.toLocaleLowerCase());
+        });
+
+        return this.state.robots.length===0 ? 
+        <h1>Loading</h1>:
+        (
+            <div className="tc">
+                <h1>Robot Friends</h1>
+                <SearchBox searchChange = {this.onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundary>
+                        <CartList robots={filteredRobots}/>
+                    </ErrorBoundary>
+                </Scroll>                
+            </div>           
+        )
+    }
+}
+
+export default App;
